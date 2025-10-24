7c478010d04b91537d766a86ee1b021ca76d5445
LeHoangMy063 <mylee060324@gmai.com>
2025-10-24 14:36:42 +0700

    Update new version of EProject Docker

---
+++ api-gateway/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const proxy = httpProxy.createProxyServer();
+const app = express();
+const morgan = require('morgan');
+
+
+
+app.use(morgan('tiny'));
+// Route requests to the auth service
+app.use("/auth", (req, res) => {
+  console.log('thành công')
+  req.url = "/auth" + req.url;
+  proxy.web(req, res, { target: "http://hoangmy_auth_service:3000" });
+});
+
+// Route requests to the product service
+app.use("/products", (req, res) => {
+  req.url = "/products" + req.url;
+  proxy.web(req, res, { target: "http://hoangmy_product_service:3001" });
+});
+
+// Route requests to the order service
+app.use("/orders", (req, res) => {
+  req.url = "/orders" + req.url;
+  proxy.web(req, res, { target: "http://hoangmy_order_service:3002" });
+});
+
+// app.use()
+
+// Start the server
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`API Gateway listening on port ${port}`);
+});
